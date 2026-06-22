57e5bd3501527de4e78f8790296b8a809ba15d9a
Farbod Parkhooi <farbodparkhooi@duck.com>
2026-06-22 16:34:52 +0330

    Update dataset.py
    Removed RandomCrop augmentation.
    Added comments.
    More readable code

--- GAN/modules/dataset.py
+++ GAN/modules/dataset.py
@@ -1,9 +1,12 @@
+# importing libraries
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import utils
 
+configs = utils.Configs()
+
+# Defining image transforms
 transform = transforms.Compose([
-    transforms.RandomCrop(size=32, padding=4, padding_mode="reflect"),
     transforms.RandomHorizontalFlip(0.5),
     transforms.RandomRotation(degrees=(0, 15), interpolation=transforms.InterpolationMode.BILINEAR, expand=False),
     # transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.1),
@@ -13,6 +16,7 @@ transform = transforms.Compose([
     # transforms.RandomErasing(p=0.5, scale=(0.02, 0.33), ratio=(0.3, 3.3), value=0, inplace=False),
 ])
 
+# Loading dataset
 dataset = datasets.CIFAR10(
     root="./",
     download=True,
@@ -20,13 +24,15 @@ dataset = datasets.CIFAR10(
     transform=transform
 )
 
+# Creating data loader
 dataloader = DataLoader(
     dataset=dataset,
-    batch_size=utils.Configs().batch_size,
+    batch_size=configs.batch_size,
     drop_last=True,
     shuffle=True,
     pin_memory=True,
-    num_workers=utils.Configs().num_workers
+    num_workers=configs.num_workers
 )
 
+# This function returns data loader
 def get_dataloader() -> DataLoader: return dataloader
