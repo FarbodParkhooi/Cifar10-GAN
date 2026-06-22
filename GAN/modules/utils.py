74ad3d38facbcc860217e9e4cf32dfafa6ff3a8f
Farbod Parkhooi <farbodparkhooi@duck.com>
2026-06-22 16:36:04 +0330

    Update utils.py
    Added dataset related values.
    Added comments

--- GAN/modules/utils.py
+++ GAN/modules/utils.py
@@ -1,7 +1,12 @@
 from dataclasses import dataclass
 
+# Creating frozen class for constant configs
 @dataclass(frozen=True)
 class Configs():
     # Generator model 
     latent_dim = 256
     projection_dim = (128, 2, 2) # Channels=128 , Width=Height=2
+
+    # Dataset
+    batch_size = 100
+    num_workers = 4
